22806b04361e9de9edd5957004979a243ac4f560
Matt Scola <scola_mb@yahoo.com>
2015-10-24 13:58:52 -0400

    Removes references to floating action buttons

--- app/src/main/java/com/uncc/gamejam49er/espionage/NewPlayerActivity.java
+++ app/src/main/java/com/uncc/gamejam49er/espionage/NewPlayerActivity.java
@@ -54,14 +54,6 @@ public class NewPlayerActivity extends AppCompatActivity {
         item_2.setImageResource(R.mipmap.ic_launcher);
 
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
--- app/src/main/java/com/uncc/gamejam49er/espionage/StartActivity.java
+++ app/src/main/java/com/uncc/gamejam49er/espionage/StartActivity.java
@@ -23,15 +23,6 @@ public class StartActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         startButton = (Button) findViewById(R.id.start_button);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
