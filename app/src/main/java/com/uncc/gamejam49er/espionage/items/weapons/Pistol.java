27627822ef0c1ece4a88ac53a22994b4fc617b4c
Matt Scola <scola_mb@yahoo.com>
2015-10-24 15:29:18 -0400

    Updates all weapons to use the new Item format

--- app/src/main/java/com/uncc/gamejam49er/espionage/items/weapons/Pistol.java
+++ app/src/main/java/com/uncc/gamejam49er/espionage/items/weapons/Pistol.java
@@ -1,12 +1,14 @@
 package com.uncc.gamejam49er.espionage.items.weapons;
 
+import com.uncc.gamejam49er.espionage.items.Item;
+
 /**
  * Created by Megan on 10/24/2015.
  */
-public class Pistol extends Weapon{
+public class Pistol extends Item {
     public Pistol() {
-        setName("Pistol");
-        setAttackValue(5);
-        setDescription("Shoot your enemy down. attack Value: " + getAttackValue());
+        this.name = "Pistol";
+        this.attackValue = 5;
+        this.description = "Shoot your enemy down.";
     }
 }
